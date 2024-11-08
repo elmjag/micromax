3dfa4169fe588edece7bd743fe54900eecff81be
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2024-11-08 08:52:19 +0100

    Dashboard: use newer version
    Install newer version of the config dashboard.
    
    Also update the emulator specific config file for dashboard, to
    specify 'start' and 'stop' commands.

--- mxcube/dboard_config.py
+++ mxcube/dboard_config.py
@@ -1,2 +1,3 @@
 MXCUBE_CONFIG_DIR="/app/conf/MicroMAX"
-MXCUBE_RESTART_COMMAND="/opt/conda/bin/circusctl restart mxcube"
+MXCUBE_START_COMMAND="/opt/conda/bin/circusctl start mxcube"
+MXCUBE_STOP_COMMAND="/opt/conda/bin/circusctl stop mxcube"
