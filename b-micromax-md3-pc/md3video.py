8c60e9aa9d49f07d154c5a2d971484db469716e4
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-02-05 14:22:08 +0100

    MD3Up: emulate OAV zoom levels
    Add support to change the zoom level of the MD3Up on-axis-video
    zoom level.
    
    The current zoom level can be changed by writing to 'video_zoom_idx'
    attribute of the OAV tango device at
    tango://b-micromax-md3-pc:9999/md/oav/bzoom#dbase=no
    
    Now the emulator sends back different frames, according to zoom
    level. Note the actual frames, captured from real MD3Up are used.
    This includes 'emulation' of the wierd monochrome frames, with
    grabage part attached at the end.

--- b-micromax-md3-pc/md3video.py
+++ b-micromax-md3-pc/md3video.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 import time
-import numpy
 import struct
-from pathlib import Path
-from random import random
+import tarfile
 from threading import Thread
 from tango.server import Device, attribute
-from simplejpeg import decode_jpeg
 
+# MD3Up OAV have 7 zoom levels
+NUM_ZOOM_LEVELS = 7
 
 CORNER_SIZE = 8
 
@@ -16,47 +15,93 @@ IMAGE_MODE_L = 0
 # rgb, 24-bit per pixel
 IMAGE_MODE_RGB = 6
 
-
-SAMPLE_JPEG = "sample.jpeg"
-IMAGE_MODE = IMAGE_MODE_RGB
 WIDTH = 1224
 HEIGHT = 1024
 HEADER_SIZE = 32
-NUM_PIXELS = WIDTH * HEIGHT
+
+#
+# number of steps between black and white we use
+# for the frame corner animation
+#
+CORNER_GRAY_LEVELS = 5
+
+FRAMES_ARCHIVE = "frames.tar.bz2"
 
 
-def _paint_corner(orig, color):
-    pixels = numpy.copy(orig)
-    for i in range(CORNER_SIZE):
-        pixels[i][0:CORNER_SIZE] = color
+def _zoom_levels():
+    """
+    returns an iterator over all supported zoom levels
+    """
+    return range(1, NUM_ZOOM_LEVELS + 1)
 
-    return bytearray(pixels)
+
+def _load_frames_from_archive():
+    frames = {}
+
+    with tarfile.open(FRAMES_ARCHIVE, mode="r:bz2") as tar:
+        for zoom_level in _zoom_levels():
+            frames[zoom_level] = tar.extractfile(f"zoom{zoom_level}").read()
+
+    return frames
 
 
-def _make_image(header, pixels, corner_color):
-    return header + _paint_corner(pixels, corner_color)
+def _paint_corner(frame: bytes, color: float):
+    def get_image_mode(frame: bytes):
+        _, __, image_mode = struct.unpack(">IHH", frame[:8])
+        return image_mode
+
+    def get_bytes_per_pixel():
+        image_mode = get_image_mode(frame)
+        if image_mode == IMAGE_MODE_L:
+            return 1
+        if image_mode == IMAGE_MODE_RGB:
+            return 3
+
+        assert False, f"unexpected image mode {image_mode}"
+
+    def get_line(bytes_per_pixel):
+        pixels = [round(255 * color)] * (CORNER_SIZE * bytes_per_pixel)
+
+        return bytes(pixels)
+
+    def get_line_indices(y, bytes_per_pixel):
+        start = y * WIDTH * bytes_per_pixel + HEADER_SIZE
+        end = start + CORNER_SIZE * bytes_per_pixel
+
+        return start, end
+
+    bytes_per_pixel = get_bytes_per_pixel()
+    line = get_line(bytes_per_pixel)
+
+    data = bytearray(frame)
+    for y in range(CORNER_SIZE):
+        start, end = get_line_indices(y, bytes_per_pixel)
+        data[start:end] = line
+
+    return data
+
+
+def _make_painted_frames(frame: bytes):
+    for color in [x / (CORNER_GRAY_LEVELS - 1) for x in range(CORNER_GRAY_LEVELS)]:
+        yield _paint_corner(frame, color)
 
 
 def _load_images():
-    sample_pixels = decode_jpeg(Path(SAMPLE_JPEG).read_bytes())
+    images = {}
+    raw_frames = _load_frames_from_archive()
 
-    header = bytearray(
-        struct.pack(
-            ">IHHqiiHHxxxx", 1447314767, 1, IMAGE_MODE, 0, WIDTH, HEIGHT, 0, HEADER_SIZE
-        )
-    )
+    for zoom_level in _zoom_levels():
+        images[zoom_level] = list(_make_painted_frames(raw_frames[zoom_level]))
 
-    yield header + bytearray(sample_pixels)
-    yield _make_image(header, sample_pixels, [0, 0, 0])
-    yield _make_image(header, sample_pixels, [255, 0, 0])
-    yield _make_image(header, sample_pixels, [0, 255, 0])
-    yield _make_image(header, sample_pixels, [0, 0, 255])
+    return images
 
 
 class MD3(Device):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._images = list(_load_images())
+
+        self._images = _load_images()
+        self._zoom_level = 1
         self._frame_number = 0
 
         #
@@ -68,7 +113,7 @@ class MD3(Device):
         Thread(target=self._increment_frame_number).start()
 
     def _get_image(self):
-        image = self._images[self._frame_number % len(self._images)]
+        image = self._images[self._zoom_level][self._frame_number % CORNER_GRAY_LEVELS]
         # update frame number in the header
         image[8:16] = struct.pack(">q", self._frame_number)
 
@@ -94,6 +139,23 @@ class MD3(Device):
     @attribute(dtype="DevULong")
     def image_height(self):
         return HEIGHT
+
+    @attribute(dtype="DevUShort")
+    def num_zoom_levels(self):
+        return NUM_ZOOM_LEVELS
+
+    #
+    # video_zoom_idx attribute
+    #
+    video_zoom_idx = attribute(dtype="DevShort")
+
+    @video_zoom_idx.getter
+    def video_zoom_idx_read(self):
+        return self._zoom_level
+
+    @video_zoom_idx.setter
+    def video_zoom_idx_write(self, val):
+        self._zoom_level = val
 
     #
     # video_live attribute
