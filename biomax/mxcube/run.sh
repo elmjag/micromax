449f93f79576b9aaf13399b5d1d7c1b189522e7a
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-10-20 10:04:13 +0200

    mamba image: use version 2.3.2
    There seems to be issues with downloading older versions of
    micromamba docker images. Use later version.
    
    Also, use the full name, as it seems to be the recommended style
    nowadays.

--- biomax/mxcube/run.sh
+++ biomax/mxcube/run.sh
@@ -2,4 +2,4 @@
 
 # stop circus managed mxcube and run new mxcube in this terminal
 /opt/conda/bin/circusctl stop mxcube
-cd /app/web && /opt/conda/bin/mxcubeweb-server --repository /app/conf/BioMAX --static-folder /opt/mxcube/build
+cd /app/web && /opt/conda/bin/mxcubeweb-server --repository /app/conf --static-folder /opt/mxcube/build
--- micromax/mxcube/run.sh
+++ micromax/mxcube/run.sh
@@ -2,4 +2,4 @@
 
 # stop circus managed mxcube and run new mxcube in this terminal
 circusctl stop mxcube
-/app/web/mxcubeweb-server --repository /app/conf/MicroMAX --static-folder /opt/mxcube/build
+/app/web/mxcubeweb-server --repository /app/conf --static-folder /opt/mxcube/build
