9b65a67dfd6a313797bcb5c9460ad42327958306
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2025-11-12 18:23:42 +0100

    adapt to newer version of MXCuBE
    Use later version of dashboard.
    
    Use new way to start MXCuBE-web process.

--- micromax/mxcube/run.sh
+++ micromax/mxcube/run.sh
@@ -2,4 +2,4 @@
 
 # stop circus managed mxcube and run new mxcube in this terminal
 circusctl stop mxcube
-/app/web/mxcubeweb-server --repository /app/conf --static-folder /opt/mxcube/build
+mxcubeweb-server --repository /app/conf --static-folder /opt/mxcube/build
